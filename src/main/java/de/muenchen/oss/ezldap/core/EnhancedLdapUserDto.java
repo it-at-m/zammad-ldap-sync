e076f74103c19819070e2d29c723972e509d7ec8
Marko Streich <marko.streich@muenchen.de>
2024-09-02 15:46:04 +0200

    MPDZ2-695 resolve linter annotations EnhancedLdapUserDto

--- src/main/java/de/muenchen/oss/ezldap/core/EnhancedLdapUserDto.java
+++ src/main/java/de/muenchen/oss/ezldap/core/EnhancedLdapUserDto.java
@@ -6,6 +6,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.io.Serial;
+
 /*
  *  @ToString(callSuper = true, onlyExplicitlyIncluded = true) : log all ldap user data
  */
@@ -17,6 +19,7 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 public class EnhancedLdapUserDto extends LdapUserDTO {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
 	private String modifyTimeStamp;
