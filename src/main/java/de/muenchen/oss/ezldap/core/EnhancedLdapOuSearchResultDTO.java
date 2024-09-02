943e57bb3bb5ef9b9255dcc7e975cefa0d96c7af
Marko Streich <marko.streich@muenchen.de>
2024-09-02 15:44:22 +0200

    MPDZ2-695 resolve linter annotations EnhancedLdapOuSearchResultDTO

--- src/main/java/de/muenchen/oss/ezldap/core/EnhancedLdapOuSearchResultDTO.java
+++ src/main/java/de/muenchen/oss/ezldap/core/EnhancedLdapOuSearchResultDTO.java
@@ -6,6 +6,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.io.Serial;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +15,7 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 public class EnhancedLdapOuSearchResultDTO extends LdapOuSearchResultDTO {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 	private String modifyTimeStamp;
 
