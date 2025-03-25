401e0315499daa4ba1a9e5a8df7bc934feffc233
sfi2022 <109145215+sfi2022@users.noreply.github.com>
2025-03-25 09:21:02 +0100

    log.warn typo

--- src/main/java/de/muenchen/zammad/ad/ldap/mediator/ActiveDirectoryGroupZammadRoleMapper.java
+++ src/main/java/de/muenchen/zammad/ad/ldap/mediator/ActiveDirectoryGroupZammadRoleMapper.java
@@ -112,7 +112,7 @@ public class ActiveDirectoryGroupZammadRoleMapper {
                         });
 
                     });
-                }, () -> log.warn("Active directory group/role '{}' not found in current zammad instance. The search applies 'group-name-pefix' : '{}'",
+                }, () -> log.warn("Active directory group/role '{}' not found in current zammad instance. The search applies 'group-name-pefix' : '{}'.",
                         adGroup.getName().replace(activeDirectoryProperties.getGroupNamePrefix(), ""), activeDirectoryProperties.getGroupNamePrefix()));
             }
         });
